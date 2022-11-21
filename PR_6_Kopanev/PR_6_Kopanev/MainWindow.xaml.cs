55c3b1fc159332d5cca8ec3b3a9457820ccb6f9b
DaniilKopanev <114130102+DaniilKopanev@users.noreply.github.com>
2022-11-21 10:33:57 +1000

    Add files via upload

---
+++ PR_6_Kopanev/PR_6_Kopanev/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PR_6_Kopanev
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (pwBox.Password.ToString() == "Bruh" && logBox.Password.ToString() == "Bruh1")
+            {
+                statusText.Text = "Верный bruh";
+                Window1 window1 = new Window1();
+                window1.Show();
+            }
+            else if (pwBox.Password.ToString() != "Bruh")
+            {
+                MessageBox.Show("Неверный пароль");
+            }
+            else if (logBox.Password.ToString() != "Bruh1")
+            {
+                MessageBox.Show("Не найден логин");
+            }
+        }
+    }
+}
